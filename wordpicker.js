6cc1a4a02b7786920efa9c87a97a31870bd6de35
penguinmoose <67451414+penguinmoose@users.noreply.github.com>
2020-10-14 17:01:37 -0700

    Another big update
    New or changed stuff:
    • new background
    • changed tab style
    • result container now shifted to the right if screen width is larger than 750 pixels
    • you now could use Word Picker on a small screen!
    bug fixes:
    element position problem on small screens

--- wordpicker.js
+++ wordpicker.js
@@ -1,5 +1,8 @@
 var host = "wordpicker-eb.eba-zkdtc4h6.us-west-2.elasticbeanstalk.com";
 
+section_list = ["word_picker", "sentence_picker", "instructions"];
+button_list = ["wd_button", "sn_button", "in_button"];
+
 console.log("welcome to word picker");
 
 function init() {
@@ -9,17 +12,27 @@ function init() {
 
   document.getElementById("resultcontainer-1").style.display = "none";
   document.getElementById("resultcontainer-2").style.display = "none";
+
+  changepage(1);
 }
 
 function changeUrl(site) {
   document.getElementsByName('resultpage')[0].src = site;
 }
 
+function changepage(pg) {
+  for (var i = 0; i < 3; i++) {
+    document.getElementById(section_list[i]).style.display = "none";
+    document.getElementById(button_list[i]).style.border = "none";
+  }
+
+  document.getElementById(section_list[pg - 1]).style.display = "block";
+  document.getElementById(button_list[pg - 1]).style.border = "20px solid #1752e8";
+}
+
 ////////////////////////////////////////////////////////
 
 function startWords() {
-  document.getElementById("resultcontainer-1").style.display = "block";
-
   var pattern = (document.getElementById("pattern").value);
   var phone = (document.getElementById("phone").value);
 
@@ -45,6 +58,7 @@ function startWords() {
       for (var i = 0; i < data.length; i++) {
         var div = document.createElement("div");
         div.innerHTML = data[i];
+        document.getElementById("resultcontainer-1").style.display = "block";
         mainContainer.appendChild(div);
       }
     }
@@ -56,8 +70,6 @@ function startWords() {
 //////////////////////////////////////////////////
 
 function startSentences() {
-  document.getElementById("resultcontainer-2").style.display = "block";
-
   var req = (document.getElementById("reqsentence").value);
   if (req !== null) {
     var url = "http://" + host + "/api/sentences?pattern=" + req;
@@ -81,6 +93,7 @@ function startSentences() {
       for (var i = 0; i < data.length; i++) {
         var div = document.createElement("div");
         div.innerHTML = data[i];
+        document.getElementById("resultcontainer-2").style.display = "block";
         mainContainer.appendChild(div);
       }
     }
