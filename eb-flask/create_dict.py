d73b19db5dcd4add7a88363872c59c26386978b6
penguinmoose <67451414+penguinmoose@users.noreply.github.com>
2021-01-21 18:12:06 -0800

    Add adjective filter and verb filter

--- eb-flask/create_dict.py
+++ eb-flask/create_dict.py
@@ -1,9 +1,10 @@
 import string
+import sys
 
 def load_phone_dict():
     dict = {}
     translation = str.maketrans(string.ascii_letters, string.ascii_letters, string.digits)
-    with open("cmudict-0.7b.txt", 'r', encoding='ISO-8859-1') as f:
+    with open("word-list/cmudict-0.7b.txt", 'r', encoding='ISO-8859-1') as f:
         for line in f:
             if line.startswith(';;;'):
                 continue
@@ -13,13 +14,13 @@ def load_phone_dict():
     return dict
 
 def load_word_list():
-    with open("10000words.txt") as f:
+    with open(sys.argv[1]) as f:
         word_list = f.read().splitlines()
     return word_list
 
 word_list = load_word_list()
 phone_dict = load_phone_dict()
-with open("word_phoneme.txt", "w") as f:
+with open(sys.argv[2], "w") as f:
     for w in word_list:
         if w in phone_dict:
             f.write(w + ":" + phone_dict[w] + '\n')
