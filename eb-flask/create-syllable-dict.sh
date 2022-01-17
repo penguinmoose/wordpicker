7552759c959a66add02f0f1ed80863a9057f756c
penguinmoose <67451414+penguinmoose@users.noreply.github.com>
2022-01-16 22:29:44 -0800

    add extra dictionary for custom words

--- eb-flask/create-syllable-dict.sh
+++ eb-flask/create-syllable-dict.sh
@@ -1,1 +1,1 @@
-cat word-list/cmudict-0.7b.txt| ./create-syllable-dict.py> word-list/syllable-dict.txt
+cat word-list/cmudict-0.7b.txt word-list/extra-dict.txt | ./create-syllable-dict.py> word-list/syllable-dict.txt
